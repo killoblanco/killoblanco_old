88f022ca7a3b575ba85c4b004c8fd4f604150093
Kamilo Vasquez <killo.blanco@gmail.com>
2021-11-14 19:51:45 -0500

    Create app.tsx

---
+++ src/core/app.tsx
@@ -0,0 +1,12 @@
+import React, { StrictMode } from 'react';
+import LandingPage from '../landing';
+import AppThemeProvider from '../styles/provider';
+
+function App() {
+  return (
+    <StrictMode>
+    </StrictMode>
+  );
+}
+
+export default App;
