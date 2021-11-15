e249f8cd0359272a11eea5eba9e50d65b790aeaa
Kamilo Vasquez <killo.blanco@gmail.com>
2021-11-15 00:09:15 -0500

    Update app.tsx

--- src/core/app.tsx
+++ src/core/app.tsx
@@ -1,13 +1,17 @@
 import React, { StrictMode } from 'react';
+import { I18nextProvider } from 'react-i18next';
 import LandingPage from '../landing';
+import i18n from '../locales';
 import AppThemeProvider from '../styles/provider';
 
 function App() {
   return (
     <StrictMode>
-      <AppThemeProvider>
-        <LandingPage />
-      </AppThemeProvider>
+      <I18nextProvider i18n={i18n}>
+        <AppThemeProvider>
+          <LandingPage />
+        </AppThemeProvider>
+      </I18nextProvider>
     </StrictMode>
   );
 }
