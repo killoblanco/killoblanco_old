3cdb781985767bb7ac911db57fdd03c404c6c9bf
Kamilo Vasquez <killo.blanco@gmail.com>
2021-11-21 17:17:13 -0500

    Update landing.tsx

--- src/seo/landing.tsx
+++ src/seo/landing.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
+import { pageview as gaPageView } from 'react-ga';
 import { Helmet } from 'react-helmet';
 
 function LandingSEO() {
+  useEffect(() => {
+    gaPageView(window.location.pathname);
+  }, []);
+
   return (
     <Helmet>
       <meta
