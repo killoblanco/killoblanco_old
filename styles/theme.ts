507e7267a04987be551ce8fd23b79f731cb3afc7
Kamilo Vasquez <killo.blanco@gmail.com>
2022-12-15 22:21:45 -0500

    Update theme.ts

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,5 +1,6 @@
 import { Readex_Pro } from '@next/font/google'
 import { createTheme, responsiveFontSizes } from '@mui/material'
+import { paletteColor } from './palette'
 
 export const readexPro = Readex_Pro({
   weight: ['400', '500', '600', '700'],
@@ -8,8 +9,9 @@ export const readexPro = Readex_Pro({
   fallback: ['sans-serif']
 })
 
-export const theme = responsiveFontSizes(
+export const theme = (darkMode: boolean) => responsiveFontSizes(
   createTheme({
+    palette: paletteColor(darkMode ? 'dark' : 'light'),
     typography: {
       fontFamily: readexPro.style.fontFamily
     }
