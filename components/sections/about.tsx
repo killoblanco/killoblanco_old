9ef08bf8fc8c9b626d04e696eb1cf647efc80821
Kamilo Vasquez <killo.blanco@gmail.com>
2022-12-17 13:56:49 -0500

    Update about.tsx

--- components/sections/about.tsx
+++ components/sections/about.tsx
@@ -20,7 +20,7 @@ const Wrapper = styled(Grid2)(({theme}) => ({
 export const About = () => {
   const { t } = useTranslation()
   return (
-    <Wrapper container spacing={0}>
+    <Wrapper component="section" container spacing={0}>
       <Grid2 xs={12} md={3} mb={8}>
         <Typography variant="h1" component="h2">
           {t('About.Header')}
