b270ca83e660868bde266e03e828fc32018cdff9
Kamilo Vasquez <killo.blanco@gmail.com>
2022-12-16 22:45:40 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import { Box, Container, Stack, Typography } from '@mui/material'
-import { useTranslation } from 'next-i18next'
+import { Container } from '@mui/material'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { Thumbnail } from '../components/thumbnail'
+import { Jumbotron } from '../components/sections/jumbotron'
+import { About } from '../components/sections/about'
+import { Skills } from '../components/sections/skills'
 
 export async function getStaticProps({ locale }: any) {
   return {
@@ -12,28 +13,11 @@ export async function getStaticProps({ locale }: any) {
 }
 
 export default function Home() {
-  const { t } = useTranslation()
   return (
     <Container maxWidth={false} disableGutters>
-      <Stack
-        direction="row"
-        justifyContent="space-evenly"
-        alignItems="center"
-        sx={{ width: '100vw', height: '100vh', overflow: 'hidden' }}
-      >
-        <Thumbnail />
-        <Box>
-          <Typography variant="h3">
-            {t('Jumbotron.Line1')}
-          </Typography>
-          <Typography variant="h1" gutterBottom>
-            {t('Jumbotron.Line2')}
-          </Typography>
-          <Typography variant="h2">
-            {t('Jumbotron.Line3')}
-          </Typography>
-        </Box>
-      </Stack>
+      <Jumbotron />
+      <About />
+      <Skills />
     </Container>
   )
 }
