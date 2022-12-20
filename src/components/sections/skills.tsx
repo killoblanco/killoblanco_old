968fc326757865fe02ee2976d0f7512c7ab11706
Kamilo Vasquez <killo.blanco@gmail.com>
2022-12-20 18:57:00 -0500

    Create skills.tsx

---
+++ src/components/sections/skills.tsx
@@ -0,0 +1,38 @@
+import { styled, Typography } from '@mui/material'
+import Grid2 from '@mui/material/Unstable_Grid2'
+import { iconList, StackIcon } from '../stack-icon'
+import { Card } from '../card'
+import { useTranslation } from 'react-i18next'
+
+const Wrapper = styled(Grid2)(({ theme }) => ({
+  alignItems: 'center',
+  justifyContent: 'space-evenly',
+  padding: theme.spacing(10, 4)
+}))
+
+export const Skills = () => {
+  const { t } = useTranslation()
+  return (
+    <Wrapper component="section" container spacing={0}>
+      <Grid2 xs={12} md={6}>
+        <Card>
+          <Grid2 container spacing={4}>
+            {iconList.map((icon) => (
+              <Grid2 xs={4} sm={3} key={icon}>
+                <StackIcon name={icon} />
+              </Grid2>
+            ))}
+          </Grid2>
+        </Card>
+      </Grid2>
+      <Grid2 xs={12} md={5} my={8}>
+        <Typography variant="h2" component="h2" gutterBottom>
+          {t('Skills.Header')}
+        </Typography>
+        <Typography variant="subtitle1" component="p" mt={2}>
+          {t('Skills.Card.Content')}
+        </Typography>
+      </Grid2>
+    </Wrapper>
+  )
+}
