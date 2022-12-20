07a104d405d2d38551edc3e8a851582b70c69ba7
Kamilo Vasquez <killo.blanco@gmail.com>
2022-12-20 18:54:44 -0500

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Core } from './core'
+
+const root = document.getElementById('root') as HTMLElement
+
+ReactDOM
+  .createRoot(root)
+  .render(<Core />)
