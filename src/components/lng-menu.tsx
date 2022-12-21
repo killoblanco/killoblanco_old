ac2da9ab2fa40a4501f6f56358990ecd20a392da
Kamilo Vasquez <killo.blanco@gmail.com>
2022-12-20 19:11:46 -0500

    Create lng-menu.tsx

---
+++ src/components/lng-menu.tsx
@@ -0,0 +1,41 @@
+import { Icon } from '@iconify/react'
+import { IconButton, Menu, MenuItem } from '@mui/material'
+import { SyntheticEvent, useState } from 'react'
+import i18n from '../locales'
+
+function LngMenu() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const openMenu = ({ currentTarget }: SyntheticEvent<HTMLButtonElement>) => {
+    setAnchorEl(currentTarget)
+  }
+
+  const closeMenu = () => {
+    setAnchorEl(null)
+  }
+
+  const setLng = (lng: string) => {
+    void i18n.changeLanguage(lng)
+    closeMenu()
+  }
+
+  return (
+    <>
+      <IconButton onClick={openMenu}>
+        <Icon icon="tabler:language" />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        open={!!anchorEl}
+        onClose={closeMenu}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <MenuItem onClick={() => setLng('es')}>Español</MenuItem>
+        <MenuItem onClick={() => setLng('en')}>English</MenuItem>
+      </Menu>
+    </>
+  )
+}
+
+export default LngMenu
