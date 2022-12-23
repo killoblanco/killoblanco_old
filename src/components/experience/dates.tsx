8d71125946d0bda8d289a0b8eb4d5963c29e5043
Kamilo Vasquez <killo.blanco@gmail.com>
2022-12-23 15:32:51 -0500

    Update dates.tsx

--- src/components/experience/dates.tsx
+++ src/components/experience/dates.tsx
@@ -1,31 +1,32 @@
 import { useTranslation } from 'react-i18next'
 import { DateTime } from 'luxon'
 import { Typography } from '@mui/material'
+import React from 'react'
 
-export const Dates = ({ date }: { date: string }) => {
+export const Dates = ({ date }: { date: string }): JSX.Element => {
   const { t, i18n } = useTranslation()
   const locale = i18n.language
   const DATE_FORMAT = 'MMM yyyy'
   const [start, end] = date.split('_')
 
-  const parseDate = (date: string | undefined) => {
+  const parseDate = (date: string | undefined): string => {
     let nextDate = DateTime.local({ locale }).toFormat(DATE_FORMAT)
-    if (date) {
+    if (date != null) {
       nextDate = DateTime.fromISO(date, { locale }).toFormat(DATE_FORMAT)
     }
     return nextDate[0].toUpperCase() + nextDate.slice(1)
   }
 
-  const getDiff = (start: string, end: string) => {
+  const getDiff = (start: string, end: string): string => {
     let endDate = DateTime.local({ locale })
-    if (end) endDate = DateTime.fromISO(end, { locale })
+    if (end !== '') endDate = DateTime.fromISO(end, { locale })
     const { years, months } = endDate.diff(
       DateTime.fromISO(start, { locale }),
       ['years', 'months']
     ).toObject()
-    const nextMonths = +(months || 0).toFixed(0)
+    const nextMonths = +(months ?? 0).toFixed(0)
 
-    if (years) {
+    if (years != null) {
       return `${t('Experience.Year', { count: years })} ${t('Experience.Month', { count: nextMonths })}`
     }
     return `${t('Experience.Month', { count: nextMonths })}`
