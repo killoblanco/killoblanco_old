c7e7eb77bc4afed54dc661daf8d290aed26efe0d
Kamilo Vasquez <killo.blanco@gmail.com>
2022-12-23 15:32:45 -0500

    Update card.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,5 +6,5 @@ export const Card = styled(Box)(({ theme }) => ({
   borderRadius: '10px',
   border: `solid ${theme.spacing(0.75)} ${theme.palette.divider}`,
   boxShadow: `${theme.spacing(1)} ${theme.spacing(1)} 0 0 ${theme.palette.info.dark}`,
-  padding: theme.spacing(6, 2),
+  padding: theme.spacing(6, 2)
 }))
