f680f2014b7f7a05d72ab7ab54b40e2aa52853cb
Kamilo Vasquez <killo.blanco@gmail.com>
2022-12-23 15:33:35 -0500

    Update experience.tsx

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -2,17 +2,18 @@ import { styled, Typography } from '@mui/material'
 import Grid2 from '@mui/material/Unstable_Grid2'
 import { List } from '../experience/list'
 import { useTranslation } from 'react-i18next'
+import React from 'react'
 
 const Wrapper = styled(Grid2)(({ theme }) => ({
   alignItems: 'flex-star',
   justifyContent: 'space-evenly',
   padding: theme.spacing(10, 6),
   [theme.breakpoints.down('md')]: {
-  padding: theme.spacing(10, 4)
+    padding: theme.spacing(10, 4)
   }
 }))
 
-export const Experience = () => {
+export const Experience = (): JSX.Element => {
   const { t } = useTranslation()
   return (
     <Wrapper component="section" container spacing={0}>
