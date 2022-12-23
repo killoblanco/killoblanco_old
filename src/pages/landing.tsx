07ca9a5352c2ce85774a6f417a6c397f4fc1cbe2
Kamilo Vasquez <killo.blanco@gmail.com>
2022-12-23 15:33:08 -0500

    Update landing.tsx

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -6,7 +6,7 @@ import { Footer } from '../components/sections/footer'
 import { Jumbotron } from '../components/sections/jumbotron'
 import { Skills } from '../components/sections/skills'
 
-export const LandingPage = () => (
+export const LandingPage = (): JSX.Element => (
   <Container component="main" maxWidth="xl" disableGutters>
     <Jumbotron />
     <About />
