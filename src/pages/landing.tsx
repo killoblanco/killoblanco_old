e0ae18efea816814078d0cec22be4e7390dbee4f
Kamilo Vasquez <killo.blanco@gmail.com>
2022-12-26 20:58:41 -0500

    Update landing.tsx

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,17 +1,24 @@
 import { Container } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { About } from '../components/sections/about'
 import { Experience } from '../components/sections/experience'
 import { Footer } from '../components/sections/footer'
 import { Jumbotron } from '../components/sections/jumbotron'
 import { Skills } from '../components/sections/skills'
+import { pageView } from '../firebase'
 
-export const LandingPage = (): JSX.Element => (
-  <Container component="main" maxWidth="xl" disableGutters>
-    <Jumbotron />
-    <About />
-    <Skills />
-    <Experience />
-    <Footer />
-  </Container>
-)
+export const LandingPage = (): JSX.Element => {
+  useEffect(() => {
+    pageView(window.location.pathname, 'Landing Page')
+  }, [])
+
+  return (
+    <Container component="main" maxWidth="xl" disableGutters>
+      <Jumbotron />
+      <About />
+      <Skills />
+      <Experience />
+      <Footer />
+    </Container>
+  )
+}
