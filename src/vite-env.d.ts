6375b3c7fdc3d53b293641c6c73bdfa33becd8a7
Kamilo Vasquez <killo.blanco@gmail.com>
2022-12-26 20:58:16 -0500

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_FIRE_API_KEY: string
+  readonly VITE_FIRE_PROJECT_ID: string
+  readonly VITE_FIRE_APP_ID: string
+  readonly VITE_FIRE_GA: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
